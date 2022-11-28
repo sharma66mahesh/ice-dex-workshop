ad68cf8d11abd3f3109ac2ca4fd95dee90e99749
sharma66mahesh <gauravpudel1990@gmail.com>
2022-11-28 13:47:02 +0545

    Update config files

--- truffle-config.js
+++ truffle-config.js
@@ -18,84 +18,101 @@
  *
  */
 
-const HDWalletProvider = require('@truffle/hdwallet-provider');
+ const HDWalletProvider = require('@truffle/hdwallet-provider');
 
-const fs = require('fs');
-const privateKey = fs.readFileSync(".secret").toString().trim();
-
-module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
-  networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-      timeoutBlocks: 200,
-    },
-    arctic: {
+ const fs = require('fs');
+ const privateKey = fs.readFileSync(".secret").toString().trim();
+ 
+ module.exports = {
+   /**
+    * Networks define how you connect to your ethereum client and let you set the
+    * defaults web3 uses to send transactions. If you don't specify one truffle
+    * will spin up a development blockchain for you on port 9545 when you
+    * run `develop` or `test`. You can ask a truffle command to use a specific
+    * network from the command line, e.g
+    *
+    * $ truffle test --network <network-name>
+    */
+ 
+   networks: {
+     // Useful for testing. The `development` name is special - truffle uses it by default
+     // if it's defined here and no other network is specified at the command line.
+     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+     // tab if you use this network and you must also set the `host`, `port` and `network_id`
+     // options below to some value.
+     //
+     development: {
       provider: () => new HDWalletProvider({
         privateKeys: [ privateKey ],
-        providerOrUrl: 'https://arctic-rpc.icenetwork.io:9933'
+        providerOrUrl: 'http://127.0.01:9933'
       }),
-      port: 9933,
+       port: 9933,            // Standard Ethereum port (default: none)
+       network_id: "*",       // Any network (default: none)
+       timeoutBlocks: 200,
+       timeoutBlocks: 2000000,
+       networkCheckTimeout: 999999,
+     },
+     arctic: {
+       provider: () => new HDWalletProvider({
+         privateKeys: [ privateKey ],
+         providerOrUrl: 'https://arctic-rpc.icenetwork.io:9933'
+       }),
+       port: 9933,
+       network_id: 552,
+       timeoutBlocks: 2000000,
+       networkCheckTimeout: 999999,
+     },
+     snow_staging: {
+      provider: () => new HDWalletProvider({
+        privateKeys: [ privateKey ],
+        providerOrUrl: 'https://temp-ice.ibriz.ai:9934'
+      }),
+      port: 9934,
       network_id: 552,
       timeoutBlocks: 2000000,
+      networkCheckTimeout: 999999,
     },
-  },
+   },
 
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
-
-  // Configure your compilers
-  compilers: {
-    solc: {
-      version: "0.8.10",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
-    }
-  },
-
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows: 
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
-    // enabled: false,
-    // host: "127.0.0.1",
-    // adapter: {
-    //   name: "sqlite",
-    //   settings: {
-    //     directory: ".db"
-    //   }
-    // }
-  // }
-};
+   // Set default mocha options here, use special reporters etc.
+   mocha: {
+     // timeout: 100000
+   },
+ 
+   // Configure your compilers
+   compilers: {
+     solc: {
+       version: "0.8.10",    // Fetch exact version from solc-bin (default: truffle's version)
+       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+       // settings: {          // See the solidity docs for advice about optimization and evmVersion
+       //  optimizer: {
+       //    enabled: false,
+       //    runs: 200
+       //  },
+       //  evmVersion: "byzantium"
+       // }
+     }
+   },
+ 
+   // Truffle DB is currently disabled by default; to enable it, change enabled:
+   // false to enabled: true. The default storage location can also be
+   // overridden by specifying the adapter settings, as shown in the commented code below.
+   //
+   // NOTE: It is not possible to migrate your contracts to truffle DB and you should
+   // make a backup of your artifacts to a safe location before enabling this feature.
+   //
+   // After you backed up your artifacts you can utilize db by running migrate as follows: 
+   // $ truffle migrate --reset --compile-all
+   //
+   // db: {
+     // enabled: false,
+     // host: "127.0.0.1",
+     // adapter: {
+     //   name: "sqlite",
+     //   settings: {
+     //     directory: ".db"
+     //   }
+     // }
+   // }
+ };
+ 
